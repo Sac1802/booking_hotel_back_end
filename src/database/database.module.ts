5dbd23a41cb4235bfaf9957fa5ea693057de6565
Said <saidacostacepeda03@gmail.com>
2026-01-17 11:05:04 -0500

    feat: It was implemented connection of the database mongodb

---
+++ src/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Module, Logger } from '@nestjs/common';
+import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (
+        config: ConfigService,
+      ): MongooseModuleFactoryOptions => {
+        const uri = config.get<string>('URL_DATABASE');
+
+        if (!uri) {
+          throw new Error('URL_DATABASE is not defined');
+        }
+
+        Logger.log('Connecting to MongoDB...', 'DatabaseModule');
+
+        return {
+          uri,
+          connectionFactory: (connection) => {
+            Logger.log(
+              'MongoDB connected successfully',
+              'DatabaseModule',
+            );
+            return connection;
+          },
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
