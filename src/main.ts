cd0624811a4168186bac1a3b6a07aed9cde7cc69
Said <saidacostacepeda03@gmail.com>
2026-02-15 11:35:58 -0500

    feat: CORS configuration added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Hotel Booking API')
     .setDescription('API documentation for reservations and rooms')
